e8d063d21a3fee403824538b6c76b7e62b835b35
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2024-02-23 18:00:46 +0100

    api ricevute

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import axios from 'axios';
+// import HelloWorld from './components/HelloWorld.vue'
+  export default{
+    methods:{
+      getProjects(){
+        axios.get('http://127.0.0.1:8000/api/projects')
+        .then(resp => console.log(resp))
+        .catch(err => console.log(err))
+      }
+    },
+    created(){
+      this.getProjects();
+    }
+  }
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
